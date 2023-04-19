48bf76bb39b41f37b8bd8684457a3f9b0cbcb413
Yakov Popov <suburbanms@gmail.com>
2023-04-19 21:13:04 +0700

    Second task completed

--- Back_One_BinaryTree/src/main/kotlin/BinaryTree&BinaryTreeNode.kt
+++ Back_One_BinaryTree/src/main/kotlin/BinaryTree&BinaryTreeNode.kt
@@ -1,91 +1,160 @@
-class BinaryTreeNode(data: Int) {
-    var data: Int
+class BinaryTreeNode(value: Int) {
+    var value: Int
     var left: BinaryTreeNode? = null
     var right: BinaryTreeNode? = null
 
     init {
-        this.data = data
+        this.value = value
     }
 }
 
-class BinaryTree() {
+class BinaryTree {
     private var node: BinaryTreeNode? = null
 
-    fun InsertItem(newItem: Int) {
+    fun insertItem(value: Int) {
 
-        var newNode = BinaryTreeNode(newItem)
+        val newNode = BinaryTreeNode(value)
 
         if (this.node == null) {
             this.node = newNode
             return
         }
-        this.InsertNextStep(node!!, newNode)
+        this.insertItemRecursion(this.node!!, newNode)
     }
 
-    fun SearchItem(item: Int) : Boolean {
-        return SearchNextStep(node!!, item)
+    fun searchItem(searchedValue: Int) : Boolean {
+        return searchItemRecursion(this.node, searchedValue)
     }
 
-    fun RemoveItem(item: Int) : Boolean {
-        return RemoveNextStep(node!!, item)
+    fun removeItem(deletedValue: Int) : Boolean {
+        return removeItemRecursion(this.node, deletedValue)
     }
 
-    private fun InsertNextStep(node: BinaryTreeNode, newItem: BinaryTreeNode) {
-
-        if (node.data > newItem.data) {
-            if (node.left == null) {
-                node.left = newItem
-            } else {
-                this.InsertNextStep(node.left!!, newItem)
-            }
-        } else if (node.data < newItem.data) {
-
-            if (node.right == null) {
-                node.right = newItem
-            } else {
-                this.InsertNextStep(node.right!!, newItem)
-            }
-        } else {
-            return println("Error: duplicate numbers.")
-        }
-    }
-
-    private fun SearchNextStep(node: BinaryTreeNode?, item: Int) : Boolean {
-        if (node == null)
-            return false
-        else if (node.data == item)
-            return true
-
-        if (node.data > item)
-            return SearchNextStep(node.left, item)
-        if (node.data < item)
-            return SearchNextStep(node.right, item)
-        return false
-    }
-
-    fun RemoveNextStep(node: BinaryTreeNode?, item: Int) : Boolean {
-        if (node == null)
-            return false
-        else if (node.data == item) {
-            if (node.left == null && node.right == null) {
-                this.node = null
-            }
-            else if (node.left != null && node.right != null){
-                this.node = node.right
-            }
+    fun getAllItem() {
+        try {
+            if (this.node == null)
+                throw Exception("Дерево не содержит значений")
             else {
-                if (node.left != null)
-                    this.node = node.left
-                if (node.right != null)
-                    this.node = node.right
+                return getAllItemRecursion(node!!)
             }
+        } catch (e: Exception) {
+            println(e)
+            return
+        }
+    }
+
+    private fun getAllItemRecursion(node: BinaryTreeNode) {
+
+        print(node.value)
+        print(' ')
+        if (node.left != null)
+            this.getAllItemRecursion(node.left!!)
+        if (node.right != null)
+            this.getAllItemRecursion(node.right!!)
+    }
+
+    private fun insertItemRecursion(node: BinaryTreeNode, newItem: BinaryTreeNode) {
+
+        try {
+            if (node.value > newItem.value) {
+                if (node.left == null) {
+                    node.left = newItem
+                } else {
+                    this.insertItemRecursion(node.left!!, newItem)
+                }
+            } else if (node.value < newItem.value) {
+
+                if (node.right == null) {
+                    node.right = newItem
+                } else {
+                    this.insertItemRecursion(node.right!!, newItem)
+                }
+            } else {
+                throw Exception("Error: duplicate numbers.")
+            }
+        } catch (e: Exception) {
+            println(e)
+            return
+        }
+    }
+
+    private fun searchItemRecursion(node: BinaryTreeNode?, searchedValue: Int) : Boolean {
+        if (node == null)
+            return false
+        else if (node.value == searchedValue)
+            return true
+
+        if (node.value > searchedValue)
+            return searchItemRecursion(node.left, searchedValue)
+        if (node.value < searchedValue)
+            return searchItemRecursion(node.right, searchedValue)
+        return false
+    }
+
+    private fun removeItemRecursion(node: BinaryTreeNode?, deletedValue: Int) : Boolean {
+        var parent: BinaryTreeNode? = null
+        var node: BinaryTreeNode? = node
+
+        while (node != null && node.value != deletedValue) {
+            parent = node
+
+            node = if (node.value > deletedValue)
+                node.left
+            else
+                node.right
+        }
+
+        if (node == null)
+            return false
+        else if (node.value == deletedValue) {
+            if (node.left == null && node.right == null) {  // Case1: у узла нет потомков
+
+                if (parent != null) {
+                    if (parent.left == node) {
+                        parent.left = null
+                    } else {
+                        parent.right = null
+                    }
+                } else {
+                    this.node = null
+                }
+            }
+            else if (node.left != null && node.right != null) {  // Case2: у узла два потомка
+                val tmp = this.gettingGreatestValueRecursion(node.left!!)
+
+                val value = tmp.value
+
+                this.removeItemRecursion(this.node, value)
+                node.value = value
+            }
+            else {                                               // Case3: у узла один потомок
+                val child: BinaryTreeNode = if (node.right != null)
+                    node.right!!
+                else
+                    node.left!!
+
+                if (parent != null) {
+                    if (parent.left == node) {
+                        parent.left = child
+                    } else {
+                        parent.right = child
+                    }
+                } else {
+                    this.node = child
+                }
+            }
+
             return true
         }
 
-        if (node.data > item)
-            return SearchNextStep(node.left, item)
-        if (node.data < item)
-            return SearchNextStep(node.right, item)
         return false
+    }
+
+    private fun gettingGreatestValueRecursion(node: BinaryTreeNode): BinaryTreeNode {
+        return if (node.right != null) {
+            gettingGreatestValueRecursion(node.right!!)
+        } else {
+            node
+        }
     }
 }
--- Back_One_BinaryTree/src/main/kotlin/Main.kt
+++ Back_One_BinaryTree/src/main/kotlin/Main.kt
@@ -1,13 +1,46 @@
 fun main(args: Array<String>) {
+/*
+    Методы для работы с деревом:
+    insertItem(Int) - вставка нового узла(если не дублируется).
+    getAllItem() - Получение всех элементов.
+    searchItem(Int) - Поиск элемента. Возвращает Boolean-значение: True в случае если значение найдено; False в случае если значение не обнаружено.
+    removeItem(Int) - Удаление элемента. Возвращает Boolean-значение: True в случае если значение удалено; False в случае если значение не обнаружено.
 
-    var binaryTree = BinaryTree()
+    Строение дерева:
+                                         5
+                                         /\
+                                        0  8
+                                        |  |\
+                                        4  7 10
+                                        /  |  |\
+                                       1   6  9 11
+                                       |
+                                       3
+                                       /
+                                      2
+*/
 
-    binaryTree.InsertItem(1)
-    binaryTree.InsertItem(2)
-    binaryTree.InsertItem(4)
-    binaryTree.InsertItem(3)
-    binaryTree.InsertItem(5)
-    binaryTree.InsertItem(0)
-    println(binaryTree.SearchItem(3))
+
+    val binaryTree = BinaryTree()
+
+    binaryTree.insertItem(5)
+    binaryTree.insertItem(0)
+    binaryTree.insertItem(8)
+    binaryTree.insertItem(4)
+    binaryTree.insertItem(1)
+    binaryTree.insertItem(10)
+    binaryTree.insertItem(7)
+    binaryTree.insertItem(3)
+    binaryTree.insertItem(11)
+    binaryTree.insertItem(2)
+    binaryTree.insertItem(6)
+    binaryTree.insertItem(9)
+
+    println(binaryTree.searchItem(3))
+    println(binaryTree.searchItem(100))
+    binaryTree.getAllItem()
+
+    println(binaryTree.removeItem(2))
+    binaryTree.getAllItem()
 
 }
